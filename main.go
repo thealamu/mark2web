9b291b40d937782a13d26be1b8533e1810a1c0d2
Faithfulness Alamu <vaguemail369@gmail.com>
2021-04-17 22:29:07 +0100

    Add function to create multipart data from a file

--- main.go
+++ main.go
@@ -1,16 +1,59 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
 	"os"
 	"strings"
 )
+
+// mPart holds multipart data
+type mPartData struct {
+	source      io.Reader
+	contentType string
+}
 
 func main() {
 	if len(os.Args) < 2 {
 		// No argument specified
 		printHelp()
 	}
+
+	filename := os.Args[1]
+	mdBytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	partData, err := createMultipart(mdBytes)
+	err = do(http.DefaultClient, partData)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// do makes a request to the server using the client
+func do(client *http.Client, mPart *mPartData) error {
+}
+
+func createMultipart(filedata []byte) (*mPartData, error) {
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+
+	fw, err := w.CreateFormFile("file", "file")
+	if err != nil {
+		return nil, err
+	}
+
+	fw.Write(filedata)
+	w.Close()
+
+	return &mPartData{&b, w.FormDataContentType()}, nil
 }
 
 func printHelp() {
