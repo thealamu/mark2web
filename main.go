434d19fdabd399195b6ed5ad6d99d3b65e1da7ae
Faithfulness Alamu <vaguemail369@gmail.com>
2021-04-17 17:18:35 +0100

    Print help on no argument

--- main.go
+++ main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
+	if len(os.Args) < 2 {
+		// No argument specified
+		printHelp()
+	}
 }
+
+func printHelp() {
+	fmt.Println(strings.TrimSpace(usageString))
+}
+
+const usageString = `
+Usage: mark2web FILE
+Renders markdown FILE as webpage, returning a URL to the webpage.
+`
